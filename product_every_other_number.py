fbeb2ca8683ffcf02448686cf31dd52346bd955e
Filippo Galli <filippo92galli@gmail.com>
2020-09-07 13:35:30 +0200

    improve solution to product of every other number

--- product_every_other_number.py
+++ product_every_other_number.py
@@ -4,17 +4,16 @@ def get_products_of_all_ints_except_at_index(int_list):
     if len(int_list) <= 2:
         raise ValueError('Need at least two other integers integer!')
 
-    first_n_products = [1]
-    last_n_products = [1]
-
     length = len(int_list)
-    for i in range(0, length-1):
-        first_n_products.append(first_n_products[i]*int_list[i])
-        last_n_products.append(last_n_products[i]*int_list[-i - 1])
-
     products = []
+    last = 1
     for i in range(0, length):
-        products.append(first_n_products[i]*last_n_products[-i-1])
+        products.append(last)
+        last *= int_list[i]
+    last = 1
+    for i in range(length - 1, -1, -1):
+        products[i] *= last
+        last *= int_list[i]
     return products
 
 
