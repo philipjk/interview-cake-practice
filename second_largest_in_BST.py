1eb18407264146a0623071eb49ca7ce34c16f68c
Filippo Galli <filippo92galli@gmail.com>
2020-09-15 17:40:13 +0200

    add solution to second largest item in BST practice exercise
    	Still messing with recursion

---
+++ second_largest_in_BST.py
@@ -0,0 +1,34 @@
+class BinaryTreeNode(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert_left(self, value):
+        self.left = BinaryTreeNode(value)
+        return self.left
+
+    def insert_right(self, value):
+        self.right = BinaryTreeNode(value)
+        return self.right
+
+
+def find_largest(node):
+    if node.right:
+        return find_largest(node.right)
+    else:
+        return node
+
+
+def find_second_largest(root_node):
+
+    # Find the second largest item in the binary search tree
+    largest = find_largest(root_node)
+    if largest.left:
+        return find_largest(largest.left).value
+    else:
+        parent = root_node
+        while parent.right.value != largest.value:
+            parent = parent.right
+        return parent.value
