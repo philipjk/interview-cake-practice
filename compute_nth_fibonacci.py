abfa53cbeddde875eb8f25282b626478a531716b
Filippo Galli <filippo92galli@gmail.com>
2020-09-20 15:16:35 +0200

    add solution to compute nth fibonacci number pt. 3
    	Amazing solution in O(log(n)) time and constant space. The trick was
    	to use powers of matrices, which can be computed (as every other power)
    	in log time. Had to look up part of the solution online though.

--- compute_nth_fibonacci.py
+++ compute_nth_fibonacci.py
@@ -1,20 +1,46 @@
 
+def power(M, p):
+
+    if p == 1:
+        return M
+
+    half = power(M, p//2)
+
+    if p % 2:
+        return mat_mat_mul(mat_mat_mul(half, half), M)
+    else:
+        return mat_mat_mul(half, half)
+
+
+def mat_mat_mul(M1, M2):
+    m11 = M1[0][0]*M2[0][0] + M1[0][1]*M2[1][0]
+    m12 = M1[0][0]*M2[0][1] + M1[0][1]*M2[1][1]
+    m21 = M1[1][0]*M2[0][0] + M1[1][1]*M2[1][0]
+    m22 = M1[1][0]*M2[0][1] + M1[1][1]*M2[1][1]
+    return [[m11, m12], [m21, m22]]
+
+
+def mat_vec_mul(M, v):
+    v1 = M[0][0]*v[0] + M[0][1]*v[1]
+    v2 = M[1][0]*v[0] + M[1][1]*v[1]
+    return [v1, v2]
+
+
 def fib(n):
 
     if n < 0:
         raise ValueError('Index must be positive')
 
-    last = 1
-    second_last = 0
+    if n == 0:
+        return 0
 
     if n == 1:
-        return last
-    if n == 0:
-        return second_last
+        return 1
 
-    for i in range(2, n + 1):
-        second_last, last = last, last + second_last
+    M = [[1, 1], [1, 0]]
+
+    vec = mat_vec_mul(power(M, n - 1), [1, 0])
 
     # Compute the nth Fibonacci number
 
-    return last
+    return vec[0]
