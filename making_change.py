1ff12f60248e58da8615703ca69f0dd39296bc4b
Filippo Galli <filippo92galli@gmail.com>
2020-09-28 16:31:23 +0200

    solution to making change practice exercise (iterative bottom-up)

--- making_change.py
+++ making_change.py
@@ -1,34 +1,15 @@
-memo = {}
-
-
-def recursive(amount, coins):
-    print(f'Checking ways to make {amount} with {coins}')
-    count = 0
-    global memo
-
-    if str((amount, coins)) in memo:
-        print('Fetched from memo')
-        return memo[str((amount, coins))]
-
-    p = amount // coins[0]
-
-    for i in range(p, -1, -1):
-        if i*coins[0] == amount:
-            count = count + 1
-        if i*coins[0] < amount:
-            delta = amount - i*coins[0]
-            if len(coins) > 1:
-                count += recursive(delta, coins[1:])
-    memo[str((amount, coins))] = count
-    return count
-
 
 def change_possibilities(amount, denominations):
     if not len(denominations):
         return 0
-    value = recursive(amount, denominations)
+    C = [0]*(amount + 1)
+    C[0] = 1
 
-    return value
+    for coin in denominations:
+        for higher_amount in range(coin, amount + 1):
+            delta = higher_amount - coin
+            C[higher_amount] += C[delta]
+    return C[amount]
 
 
-print(change_possibilities(4, [3, 1, 2]))
+print(change_possibilities(50, [5, 10]))
